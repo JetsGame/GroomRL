912c705f078a71b73e45dc545178b9ad43f2d317
Frederic Alexandre Dreyer <frederic.dreyer@cern.ch>
2018-10-23 09:15:48 +0200

    added command line argument support

--- python/MLGroomer.py
+++ python/MLGroomer.py
@@ -9,7 +9,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, LSTM, Dropout
 from keras.optimizers import Adam
 
-import os
+import os, argparse
 
 def model_construct(hps):
     """Construct a neural network to use with the DQN agent."""
@@ -49,14 +49,13 @@ def dqn_construct(hps):
     
     return agent
 
-def run_model(network='Dense', fn = '../constit-long.json.gz',
-              nev=400000, outfn=None):
+def run_model(network, fn, nev, mass, width, nstep, outfn=None):
     """Run a test model"""
 
     # set up environment
     env = GroomEnv(fn, nev, outfn=outfn, low=np.array([0.0, -6.0]),
-                   high=np.array([10.0, 8.0]), mass=80.385,
-                   target_prec = 0.05, mass_width = 1.0)
+                   high=np.array([10.0, 8.0]), mass=mass,
+                   target_prec = 0.05, mass_width = width)
 
     # hyperparameters
     dqn_hps = {
@@ -64,14 +63,14 @@ def run_model(network='Dense', fn = '../constit-long.json.gz',
         'nb_actions': 2,
         'architecture': network,
         'description': 'test model',
-        'model_name': 'DQN_Test_Model_%s' % network
+        'model_name': 'DQN_%s_nev%i_nstep%i' % (network, nev, nstep)
     }
 
     print('Constructing DQN agent...')
     dqn = dqn_construct(dqn_hps)
 
     print('Fitting DQN agent...')
-    dqn.fit(env, nb_steps=500000, visualize=False, verbose=1)
+    dqn.fit(env, nb_steps=nstep, visualize=False, verbose=1)
 
     print('Saving weights...')
     # After training is done, we save the final weights.
@@ -80,9 +79,22 @@ def run_model(network='Dense', fn = '../constit-long.json.gz',
     return dqn
     
 if __name__ == "__main__":
-    network='Dense'
+    # read command line arguments
+    parser = argparse.ArgumentParser(description='Train an ML groomer.')
+    parser.add_argument('--lstm',action='store_true',dest='lstm')
+    parser.add_argument('--nev',type=int, default=500000,dest='nev')
+    parser.add_argument('--nstep',type=int, default=500000,dest='nstep')
+    parser.add_argument('--file',action='store',default='../constit-long.json.gz',dest='fn')
+    parser.add_argument('--massgoal',type=float, default=80.385,dest='mass')
+    parser.add_argument('--masswidth',type=float, default=1.0,dest='width')
+    args = parser.parse_args()
+
+    if args.lstm:
+        network='LSTM'
+    else:
+        network='Dense'
     # create the DQN agent and train it.
-    dqn = run_model(network=network)
+    dqn = run_model(network, args.fn, args.nev, args.mass, args.width, args.nstep)
 
     fnres = 'test_%s.pickle' % network
     # create an environment for the test sample
